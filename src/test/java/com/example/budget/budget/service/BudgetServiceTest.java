c92d5ce1ed47977841c0dba46eab10d4dd501a05
9898s <sueh9898@icloud.com>
2023-11-11 02:39:40 +0900

    test: 예산 수정 테스트

--- src/test/java/com/example/budget/budget/service/BudgetServiceTest.java
+++ src/test/java/com/example/budget/budget/service/BudgetServiceTest.java
@@ -6,7 +6,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
+import com.example.budget.budget.dto.BudgetRequestDto.BudgetEditRequestDto;
 import com.example.budget.budget.dto.BudgetRequestDto.BudgetSetRequestDto;
+import com.example.budget.budget.dto.BudgetResponseDto.BudgetEditResponseDto;
 import com.example.budget.budget.dto.BudgetResponseDto.BudgetSetResponseDto;
 import com.example.budget.budget.entity.Budget;
 import com.example.budget.budget.repository.BudgetRepository;
@@ -15,6 +17,7 @@ import com.example.budget.category.service.CategoryService;
 import com.example.budget.global.handler.exception.CustomApiException;
 import com.example.budget.member.entity.Member;
 import com.example.budget.member.service.MemberService;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -117,5 +120,55 @@ class BudgetServiceTest {
     });
 
     assertEquals("이미 존재하는 데이터입니다.", exception.getMessage());
+  }
+
+  @Test
+  void 예산수정_test() throws Exception {
+    // given
+    Member hong = Member.builder()
+        .id(1L)
+        .account("hong")
+        .password("1234")
+        .build();
+
+    Category home = Category.builder()
+        .id(1L)
+        .name("주거")
+        .build();
+
+    Category food = Category.builder()
+        .id(2L)
+        .name("식비")
+        .build();
+
+    Budget budget = Budget.builder()
+        .id(1L)
+        .amount(1000L)
+        .member(hong)
+        .category(home)
+        .build();
+
+    BudgetEditRequestDto requestDto = new BudgetEditRequestDto();
+    requestDto.setAmount(2000L);
+    requestDto.setCategoryId(2L);
+
+    // stub 1
+//    when(memberService.getMemberEntity(anyLong())).thenReturn(hong);
+
+    // stub 2
+    when(categoryService.getCategoryId(anyLong())).thenReturn(food);
+
+    // stub 3
+//    when(budgetRepository.existsByMemberAndCategory(any(), any())).thenReturn(false);
+
+    // stub 4
+    when(budgetRepository.findById(anyLong())).thenReturn(Optional.ofNullable(budget));
+
+    // when
+    BudgetEditResponseDto responseDto = budgetService.editBudget(1L, requestDto);
+
+    // then
+    assertEquals(2000L, responseDto.getAmount());
+    assertEquals("식비", responseDto.getCategoryName());
   }
 }
