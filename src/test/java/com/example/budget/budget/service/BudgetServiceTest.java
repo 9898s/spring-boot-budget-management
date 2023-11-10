9f2e6a0959f5274f344c5414b68c3e392db9b811
9898s <sueh9898@icloud.com>
2023-11-11 04:18:22 +0900

    test: 에산 추천 테스트

--- src/test/java/com/example/budget/budget/service/BudgetServiceTest.java
+++ src/test/java/com/example/budget/budget/service/BudgetServiceTest.java
@@ -7,8 +7,10 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 import com.example.budget.budget.dto.BudgetRequestDto.BudgetEditRequestDto;
+import com.example.budget.budget.dto.BudgetRequestDto.BudgetRecommendRequestDto;
 import com.example.budget.budget.dto.BudgetRequestDto.BudgetSetRequestDto;
 import com.example.budget.budget.dto.BudgetResponseDto.BudgetEditResponseDto;
+import com.example.budget.budget.dto.BudgetResponseDto.BudgetRecommendResponseDto;
 import com.example.budget.budget.dto.BudgetResponseDto.BudgetSetResponseDto;
 import com.example.budget.budget.entity.Budget;
 import com.example.budget.budget.repository.BudgetRepository;
@@ -17,6 +19,8 @@ import com.example.budget.category.service.CategoryService;
 import com.example.budget.global.handler.exception.CustomApiException;
 import com.example.budget.member.entity.Member;
 import com.example.budget.member.service.MemberService;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -170,5 +174,42 @@ class BudgetServiceTest {
     // then
     assertEquals(2000L, responseDto.getAmount());
     assertEquals("식비", responseDto.getCategoryName());
+  }
+
+  @Test
+  void 예산추천_test() throws Exception {
+    // given
+    Member kim = Member.builder().id(1L).account("kim").password("1234").build();
+    Member lee = Member.builder().id(2L).account("lee").password("1234").build();
+
+    Category home = Category.builder().id(1L).name("주거").build();
+    Category food = Category.builder().id(2L).name("식비").build();
+    Category traffic = Category.builder().id(3L).name("교통").build();
+
+    Budget budget1 = Budget.builder().id(1L).amount(300L).member(kim).category(home).build();
+    Budget budget2 = Budget.builder().id(2L).amount(300L).member(lee).category(home).build();
+    Budget budget3 = Budget.builder().id(3L).amount(150L).member(kim).category(food).build();
+    Budget budget4 = Budget.builder().id(4L).amount(150L).member(lee).category(food).build();
+    Budget budget5 = Budget.builder().id(5L).amount(100L).member(kim).category(traffic).build();
+
+    List<Budget> budgets = Arrays.asList(budget1, budget2, budget3, budget4, budget5);
+
+    BudgetRecommendRequestDto requestDto = new BudgetRecommendRequestDto();
+    requestDto.setAmount(10000L);
+    requestDto.setMemberId(3L);
+
+    // stub 1
+//    when(memberService.getMemberEntity(anyLong())).thenReturn(hong);
+
+    // stub 2
+    when(budgetRepository.findAllWithCategory()).thenReturn(budgets);
+
+    // when
+    BudgetRecommendResponseDto responseDto = budgetService.recommendBudget(requestDto);
+
+    // then
+    assertEquals(6000L, responseDto.getCategoryRecommend().get("주거"));
+    assertEquals(3000L, responseDto.getCategoryRecommend().get("식비"));
+    assertEquals(1000L, responseDto.getCategoryRecommend().get("기타"));
   }
 }
